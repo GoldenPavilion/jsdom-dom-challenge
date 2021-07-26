d16037188562b44f75ef88125e4951353564df92
GoldenPavilion <rcorystine@gmail.com>
2021-07-26 18:07:04 -0400

    challenge complete

--- js/challenge.js
+++ js/challenge.js
@@ -1,13 +1,20 @@
 setTimeout(start, 1000);
 
+let interval = setInterval(increase, 1000);
 let i = 0;
 let count = document.getElementById("counter");
 let subtract = document.getElementById("minus");
 let add = document.getElementById("plus");
 let like = document.getElementById("heart");
+let likedNumbers = {}
+let likeList = document.querySelector("ul.likes")
+let pause = document.getElementById("pause");
+let submit = document.getElementById("submit");
+let commentForm = document.getElementById("comment-form");
+let commentList = document.getElementById("list");
 
 function start(){
-    setInterval(increase, 1000);
+    interval;
 }
 
 function increase(){
@@ -28,10 +35,47 @@ add.addEventListener("click", function(){
     increase();
 })
 
-like.addEventListener("click", function(){
-    let likeList = document.getElementsByClassName("likes");
+like.addEventListener("click", function(){ 
     let item = document.createElement("li");
-    
-    item.innerText = `${i} Liked`;
-    likeList.appendChild(item);
+
+    if (likedNumbers[i]){
+        likedNumbers[i]+=1;
+        item.innerText = `${i} Like ${likedNumbers[i]} Times.`
+        likeList.appendChild(item);
+    } else {
+        likedNumbers[i] = 1;   
+        item.innerText = `${i} Liked Once`;
+        likeList.appendChild(item);
+    }
 })
+
+pause.addEventListener("click", function(){
+    if (pause.innerText == "pause"){
+        clearInterval(interval)
+        pause.innerText = "resume";
+        subtract.disabled = true;
+        add.disabled = true;
+        like.disabled = true;
+        submit.disabled = true;
+    } else {
+        setInterval(increase, 1000);
+        pause.innerText = "pause";
+        subtract.disabled = false;
+        add.disabled = false;
+        like.disabled = false;
+        submit.disabled = false;
+    }
+})
+
+commentForm.addEventListener("submit", function(e){
+    e.preventDefault();    
+    let newComment = document.getElementById("comment-input").value;
+    let listItem = document.createElement("li");
+    if (newComment != ""){
+        listItem.innerText = newComment;
+        commentList.appendChild(listItem);
+    }
+})
+
+
+
